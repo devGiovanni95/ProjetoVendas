153d5ebf6905be978f6dda933ba06b74f9dfaadd
devGiovanni95 <giovanni.devjava@hotmail.com>
2021-11-20 14:51:36 -0300

    Criando itens nos menus criados anteriormente

--- src/main/java/br/com/projeto/view/FrmMenu.java
+++ src/main/java/br/com/projeto/view/FrmMenu.java
@@ -28,27 +28,95 @@ public class FrmMenu extends javax.swing.JFrame {
 
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
         jMenu3 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenu7 = new javax.swing.JMenu();
+        jMenuItem8 = new javax.swing.JMenuItem();
+        jMenuItem9 = new javax.swing.JMenuItem();
         jMenu4 = new javax.swing.JMenu();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem6 = new javax.swing.JMenuItem();
         jMenu5 = new javax.swing.JMenu();
+        jMenuItem7 = new javax.swing.JMenuItem();
+        jMenu6 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Sistema de Controle de Vendas");
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowActivated(java.awt.event.WindowEvent evt) {
+                formWindowActivated(evt);
+            }
+        });
 
+        //jMenu1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/imagens/clientes.png"))); // NOI18N
         jMenu1.setText("Clientes");
+
+        jMenuItem1.setText("Controle de Clientes");
+        jMenu1.add(jMenuItem1);
+
         jMenuBar1.add(jMenu1);
 
+       // jMenu2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/imagens/funcionarios.png"))); // NOI18N
         jMenu2.setText("Funcionários");
+
+        jMenuItem2.setText("Controle de Funcionários");
+        jMenu2.add(jMenuItem2);
+
         jMenuBar1.add(jMenu2);
 
+        //jMenu3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/imagens/fornecedores.png"))); // NOI18N
         jMenu3.setText("Fornecedores");
+
+        jMenuItem3.setText("Controle de Fornecedores");
+        jMenu3.add(jMenuItem3);
+
         jMenuBar1.add(jMenu3);
 
+        //jMenu7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/imagens/produtos.png"))); // NOI18N
+        jMenu7.setText("Produtos");
+
+        jMenuItem8.setText("Controle de  Estoque");
+        jMenu7.add(jMenuItem8);
+
+        jMenuItem9.setText("Consulta de Produtos");
+        jMenu7.add(jMenuItem9);
+
+        jMenuBar1.add(jMenu7);
+
+        //jMenu4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/imagens/vendas.png"))); // NOI18N
         jMenu4.setText("Vendas");
+
+        jMenuItem4.setText("Abrir PDV");
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
+        jMenu4.add(jMenuItem4);
+
+        jMenuItem5.setText("Posição do dia");
+        jMenu4.add(jMenuItem5);
+
+        jMenuItem6.setText("Histórico de Vendas");
+        jMenu4.add(jMenuItem6);
+
         jMenuBar1.add(jMenu4);
 
+        //jMenu5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/imagens/configuracoes.png"))); // NOI18N
         jMenu5.setText("Configurações");
+
+        jMenuItem7.setText("Trocar de Usuário");
+        jMenu5.add(jMenuItem7);
+
         jMenuBar1.add(jMenu5);
+
+       // jMenu6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/imagens/sair.png"))); // NOI18N
+        jMenu6.setText("Sair");
+        jMenuBar1.add(jMenu6);
 
         setJMenuBar(jMenuBar1);
 
@@ -60,11 +128,23 @@ public class FrmMenu extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 511, Short.MAX_VALUE)
+            .addGap(0, 479, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem4ActionPerformed
+
+    private void formWindowActivated(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowActivated
+        // Deixar em tela cheia
+        
+        this.setExtendedState(this.MAXIMIZED_BOTH);
+        this.setVisible(true);
+
+    }//GEN-LAST:event_formWindowActivated
 
     /**
      * @param args the command line arguments
@@ -77,7 +157,7 @@ public class FrmMenu extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
@@ -107,6 +187,17 @@ public class FrmMenu extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenu jMenu7;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JMenuItem jMenuItem8;
+    private javax.swing.JMenuItem jMenuItem9;
     // End of variables declaration//GEN-END:variables
 }
