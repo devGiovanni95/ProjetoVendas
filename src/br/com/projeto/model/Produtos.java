2f83f3892512ed96146d176dee582b0edfa71033
devGiovanni95 <giovanni.devjava@hotmail.com>
2021-11-22 19:35:18 -0300

    Criando modelo de Produto para efetuar o cadastramento de novos produtos

---
+++ src/br/com/projeto/model/Produtos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.projeto.model;
+
+/**
+ *
+ * @author geu_p
+ */
+public class Produtos {
+    
+    //Atributos 
+    private int id;
+    private String descrição;
+    private double preco;
+    private int qtd_estoque;
+    
+    private Fornecedores fornecedor;
+    
+    //Getters e Setters
+    
+    
+}
